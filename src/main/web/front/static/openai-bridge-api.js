0dee24596621c7279bdc8b853bd5e99ef3956b34
Sinri Edogawa <ljni@leqee.com>
2023-03-02 15:52:58 +0800

    chat html

--- src/main/web/front/static/openai-bridge-api.js
+++ src/main/web/front/static/openai-bridge-api.js
@@ -1,12 +1,19 @@
 const call_bridge_api = function (api, post_body, pass_id, pass_secret, onSuccess, onFailure) {
     let now = Math.floor(Date.now() / 1000);
-    axios.post('../bridge' + api, post_body, {
-        'X-BRIDGE-PASS-TOKEN': md5(pass_id + ':' + pass_secret + '@' + now),
-        'X-BRIDGE-PASS-ID': pass_id,
-        'X-BRIDGE-PASS-TIME': now,
-    }).then(resp => {
-        onSuccess(resp);
-    }).catch(error => {
-        onFailure(error);
-    });
+    axios({
+        method: 'POST',
+        headers: {
+            'X-BRIDGE-PASS-TOKEN': md5(pass_id + ':' + pass_secret + '@' + now),
+            'X-BRIDGE-PASS-ID': pass_id,
+            'X-BRIDGE-PASS-TIME': now,
+        },
+        data: post_body,
+        url: '../bridge' + api,
+    })
+        .then(resp => {
+            onSuccess(resp);
+        })
+        .catch(error => {
+            onFailure(error);
+        });
 }
